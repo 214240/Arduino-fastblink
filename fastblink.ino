cfe9c4096fb4e071f75749f03bfda91311c68e01
214240 <42417458+214240@users.noreply.github.com>
2018-08-22 11:18:38 -0400

    Add files via upload

---
+++ fastblink.ino
@@ -0,0 +1,44 @@
+void setup() {
+  pinMode(LED_BUILTIN, OUTPUT);
+}
+void loop() {
+  digitalWrite(LED_BUILTIN, HIGH);  
+  delay(1000);                      
+  digitalWrite(LED_BUILTIN, LOW);   
+  delay(900); 
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(800);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(700);
+  digitalWrite(LED_BUILTIN, HIGH); 
+  delay(600);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(500);
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(400);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(300);
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(200);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(100);
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(90);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(80);
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(70);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(60);
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(50);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(40)
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(30)
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(20)
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(10)
+  digitalWrite(LED_BUILTIN, LOW);
+}
